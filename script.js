58eb55518304458d784bc5e2f161cbb0f3a399c4
AbixSol <ssolomka@gmail.com>
2022-08-13 11:04:44 +0200

    Rempved comas from Arrey of JSON.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const bookingsList = document.querySelector(".bookings_list");
-const bookingsHead = document.querySelector(".bookings_head");
+const bookingsList = document.querySelector('.bookings_list');
+const bookingsHead = document.querySelector('.bookings_head');
 
-let day = "2002-12-09T00:00:00.000Z";
+let day = '2002-12-09T00:00:00.000Z';
 
 const getAllBookings = async function () {
-  const response = await fetch("http://localhost:5000/bookings/");
-  if (!response.ok) throw new Error("Something  wrong");
+  const response = await fetch('http://localhost:5000/bookings/');
+  if (!response.ok) throw new Error('Something  wrong');
   const data = await response.json();
   return data;
 };
@@ -28,22 +28,19 @@ const renderBookings = function (bookingsObj) {
 ${bookingsObj
   .filter((book) => book.date === day)
   .map((book) => {
-    return `
-      
-      <tr>
+    return `      <tr>
         <th scope="row">${book.date}</th>
         <td>${book.time}</td>
         <td>${book.amount}</td>
         <td>${book.name}</td>
         <td>${book.requests}</td>
         <td>${book.contact} </td>
-        <td> <button class="btn btn-secondary btn-booking id = ${book._id}" >OPEN</button></td>
-      </tr>  
-  `;
-  })}
+        <td> <button class="btn btn-secondary btn-booking id = ${book._id}" >OPEN</button></td></tr>`;
+  })
+  .join('\n')}
   </tbody>
   </table> `;
-  bookingsList.insertAdjacentHTML("afterbegin", markup);
+  bookingsList.insertAdjacentHTML('afterbegin', markup);
 };
 
 getAllBookings().then((data) => renderBookings(data));
